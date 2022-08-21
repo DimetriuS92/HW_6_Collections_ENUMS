aa45ed6b2281520cb524369d9dc6c141fa227dc5
DimetriuS92 <Fakestyle92@gmail.com>
2022-08-22 02:48:35 +0300

    first task

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    public static final Logger LOGGER = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+        LOGGER.info("Hello world!");
+    }
+}
---
+++ src/main/java/org/example/listsTask/ListTask.java
@@ -0,0 +1,48 @@
+package org.example.listsTask;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+
+public class ListTask {
+    public static final Logger LOGGER = LogManager.getLogger(ListTask.class);
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<String> lists = new ArrayList<>();
+        LOGGER.error("Enter words until you enter the word - break");
+        LOGGER.error("Enter word:");
+        String name = in.nextLine();
+        while (!Objects.equals(name, "break")){
+            lists.add(name);
+            LOGGER.error("Enter word:");
+            name = in.nextLine();
+        }
+
+        LOGGER.error("\n~~~~~~Task 1~~~~~~");
+        for(String list : lists){
+            LOGGER.error(list);
+        }
+
+        LOGGER.error("\n~~~~~~Task 2~~~~~~");
+        for (String word : lists){
+            if (word.startsWith("s")){
+                LOGGER.error(word);
+            }
+        }
+
+        LOGGER.error("\n~~~~~~Task 3~~~~~~");
+        for (String word : lists) {
+            if(word.length()>5) {
+                LOGGER.error(word);
+            }
+        }
+    }
+}
+
+
